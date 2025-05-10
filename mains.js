98a9a6a05fd2d3d420edc8c33c3274ae040cbb41
romanMamrukov <roman.mamrukov@gmail.com>
2025-05-10 18:40:44 +0300

    update nav menu script

--- mains.js
+++ mains.js
@@ -31,3 +31,31 @@ document.addEventListener('DOMContentLoaded', () => {
         foodMenu.classList.toggle('hidden', isDrinks);
     };
 });
+
+// curten menu
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+// Attach event listener to nav links inside the curtain menu
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll("#myNav a");
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault(); // Prevent default anchor behavior
+      const targetId = this.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+      
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: "smooth" });
+      }
+
+      closeNav(); // Close the curtain menu
+    });
+  });
+});
